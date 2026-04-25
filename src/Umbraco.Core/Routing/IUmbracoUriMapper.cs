1565cf00c04cd1e8ef421bfc9d820ada09f2f908
Laird Popkin <laird.popkin@parthenon.ey.com>
2026-04-25 16:37:20 -0400

    feat(routing): add IUmbracoUriMapper interface
    Defines the URI-mapping contract that will be split off from UriUtility.
    Exposes its own PathResolver so consumers don't need a separately
    injected IApplicationPathResolver.

---
+++ src/Umbraco.Core/Routing/IUmbracoUriMapper.cs
@@ -0,0 +1,37 @@
+// Copyright (c) Umbraco.
+// See LICENSE for more details.
+
+using Umbraco.Cms.Core.Configuration.Models;
+
+namespace Umbraco.Cms.Core.Routing;
+
+/// <summary>
+///     Maps URIs between Umbraco-internal form (no virtual directory, lowercased) and public form.
+/// </summary>
+/// <remarks>
+///     Implementations are stateless aside from their injected <see cref="IApplicationPathResolver" />
+///     and safe to use as singletons.
+/// </remarks>
+public interface IUmbracoUriMapper
+{
+    /// <summary>
+    ///     The application path resolver this mapper was constructed with. Exposed so that consumers
+    ///     of the mapper do not need a separately injected resolver instance.
+    /// </summary>
+    IApplicationPathResolver PathResolver { get; }
+
+    /// <summary>
+    ///     Maps an internal Umbraco URI to a public URI with virtual directory and appropriate suffixes.
+    /// </summary>
+    Uri UriFromUmbraco(Uri uri, RequestHandlerSettings requestConfig);
+
+    /// <summary>
+    ///     Maps a media Umbraco URI to a public URI with virtual directory.
+    /// </summary>
+    Uri MediaUriFromUmbraco(Uri uri);
+
+    /// <summary>
+    ///     Maps a public URI to an internal Umbraco URI without virtual directory, lowercased.
+    /// </summary>
+    Uri UriToUmbraco(Uri uri);
+}
